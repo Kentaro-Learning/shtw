8c3517ce6fe07732c8a367e6ed45812e69e164d1
kentaro.hara <pski39432@gmail.com>
2018-09-23 05:10:24 +0000

    upd_env

--- database/migrations/2018_09_23_122015_users_upd.php
+++ database/migrations/2018_09_23_122015_users_upd.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UsersUpdate extends Migration
+class UsersUpd extends Migration
 {
     /**
      * Run the migrations.
